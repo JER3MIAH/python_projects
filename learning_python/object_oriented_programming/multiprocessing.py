09791678c2e030d1858531beb5fd1fce601b4f93
JER3MIAH <jeremiahbrownie@gmail.com>
2023-08-05 13:06:23 +0100

    learned some more python

---
+++ learning_python/object_oriented_programming/multiprocessing.py
@@ -0,0 +1,7 @@
+# ***********************************
+# Python multiprocessing
+# ***********************************
+# multiprocessing = running tasks in parallel on different cpu cores, bypasses GIL used for threading
+#                   multiprocessing = better for cpu bound tasks (heavy cpu usage)
+#                   multithreading = better for io bound tasks (waiting around)
+# ***********************************

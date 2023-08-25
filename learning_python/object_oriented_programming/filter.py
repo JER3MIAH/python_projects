fa64551b4c094b44ecdc890abfb7301dc0267cbb
JER3MIAH <jeremiahbrownie@gmail.com>
2023-08-25 13:15:34 +0100

    learned to use the filter function

--- learning_python/object_oriented_programming/filter.py
+++ learning_python/object_oriented_programming/filter.py
@@ -2,3 +2,17 @@
 #               for which a function returns true
 #
 #               filter(function, iterable)
+
+friends = [("Jeremiah", 20),
+           ("Ololade", 18),
+           ("Blade", 17),
+           ("James", 16),
+           ("Henry", 28),
+           ("Bones", 25)]
+
+age = lambda data: data[1] >= 18
+
+drinking_buds = list(filter(age, friends))
+
+for i in drinking_buds:
+    print(i)

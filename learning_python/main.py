6501bad03d8523dbfbfd9b6f67abc245898f4467
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-09 16:57:32 +0100

    classes practice

--- learning_python/main.py
+++ learning_python/main.py
@@ -1,12 +1,20 @@
-class PlayerCharacter:
-    def __init__(self, name) -> None:
+#Given the below class:
+class Cat:
+    species = 'mammal'
+    def __init__(self, name, age):
         self.name = name
-
-    def run():
-        print("running")
+        self.age = age
 
 
-player1 = PlayerCharacter
+# 1 Instantiate the Cat object with 3 cats
+cat1 = Cat('Jeremiah', 20)
+cat2 = Cat('jojo', 30)
+cat3 = Cat('Joseph', 50)
 
-player2 = player1('JOJOO')
-print(player2.name)
+# 2 Create a function that finds the oldest cat
+def oldestCat(*cats):
+    return max(cats)
+
+oldest = oldestCat(cat1.age, cat2.age, cat3.age)
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f'The oldest cat is {oldest} years old')

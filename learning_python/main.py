8027c844cad1f1ae1de27550d8591b630af19137
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-10 19:18:27 +0100

    practice

--- learning_python/main.py
+++ learning_python/main.py
@@ -1,3 +1,13 @@
-my_list = [1, 2, 3]
-my_dict = {num: num*2 for num in my_list}
-print(my_dict)
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+# duplicates = []
+# for value in some_list:
+#     if some_list.count(value) > 1:
+#         if value not in duplicates:
+#             duplicates.append(value)
+
+# print(duplicates)
+
+duplicates = list(set([x for x in some_list if some_list.count(x) > 1]))
+ 
+print(duplicates)

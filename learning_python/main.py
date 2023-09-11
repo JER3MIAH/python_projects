0ed668ead33d967163f9089342461c6e6c788df5
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-11 10:42:11 +0100

    decorators in python

--- learning_python/main.py
+++ learning_python/main.py
@@ -1,13 +1,12 @@
-some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+def my_decorator(func):
+    def wrap_func():
+        print('*******')
+        func()
+        print('*******')
+    return wrap_func
 
-# duplicates = []
-# for value in some_list:
-#     if some_list.count(value) > 1:
-#         if value not in duplicates:
-#             duplicates.append(value)
+@my_decorator
+def hello():
+    print('Jojooooooo')
 
-# print(duplicates)
-
-duplicates = list(set([x for x in some_list if some_list.count(x) > 1]))
- 
-print(duplicates)
+hello()

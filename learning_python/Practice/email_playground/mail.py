ee16bd1498420722c0e042bc611c756396093926
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-14 11:04:41 +0100

    Sending custom emails with python

--- learning_python/Practice/email_playground/mail.py
+++ learning_python/Practice/email_playground/mail.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Owoade Jeremiah'
 email['to'] = 'jeremiahbrownie@gmail.com'
 email['subject'] = 'You have won a 1,000,000 dollars!'
 
-email.set_content('I am a python developer')
+email.set_content(html.substitute({'name': 'TinTin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
